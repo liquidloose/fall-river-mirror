b1948fbdfb63a3030856130fac55963da994bfae
liquidloose <ronaldlussier83@gmail.com>
2025-07-20 20:03:41 +0000

    Fixed configuration issues related to directory ownership and git

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from typing import Union
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,8 +7,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-
-    return {"Hello": "World"}
+    x = 5555555555555555555555555555555555555555555555555555555
+    xai_api_key = os.getenv("XAI_API_KEY")
+    return {"XAI_API_KEY": f"{xai_api_key}"}
 
 
 @app.get("/items/{item_id}")
