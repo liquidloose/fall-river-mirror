e2793e0de544e1ef488276fce78327430b4e05f1
liquidloose <ronaldlussier83@gmail.com>
2025-07-22 04:15:09 +0000

    Started on the POST requests in Python

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 import os
+import requests
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,9 +8,20 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    x = 5555555555555555555555555555555555555555555555555555555
     xai_api_key = os.getenv("XAI_API_KEY")
-    return {"XAI_API_KEY": f"{xai_api_key}"}
+    url = "http://192.168.1.17:9004/wp-json/fr-mirror/v2/create-article"
+
+    payload = "title=THIS%20IS%20RONALD&content=%3Cdiv%3E%20Hello%2C%20worlllllld!%20%3C%2Fdiv%3E&status=draft"
+    headers = {
+        "Authorization": f"Bearer {xai_api_key}",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+    return {"response": response.text}
 
 
 @app.get("/items/{item_id}")
